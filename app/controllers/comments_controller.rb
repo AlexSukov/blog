263b6b3740770c672019803b85ce3e20b99cdc74
vakyl9r <proskater.2008@gmail.com>
2016-11-05 19:56:25 +0300

    #6 Made new post page and edit post page. Installed responders, modified controllers accordingly. Converted post pages to haml. Minor addition to stylesheets

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
+  respond_to :json, :html
   # GET /comments
   # GET /comments.json
   def index
     @comments = Comment.all
+    respond_with(@comments)
   end
 
   # GET /comments/1
@@ -15,6 +16,7 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     @comment = Comment.new
+    respond_with(@comment)
   end
 
   # GET /comments/1/edit
@@ -24,41 +26,22 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment = Comment.create(comment_params)
+    respond_with(@comment)
   end
 
   # PATCH/PUT /comments/1
   # PATCH/PUT /comments/1.json
   def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment.update(comment_params)
+    respond_with(@comment)
   end
 
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@comment)
   end
 
   private
