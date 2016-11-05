f1fea20191ef95cf41b2bb8f267e7e9eae310df3
Alex Sukov <sukovsasha@gmail.com>
2016-11-05 14:09:10 +0300

    #5 Created main page with showing all posts. Converted to haml/ Added route to main apge

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   respond_to :json, :html
 
