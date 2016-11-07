c9a4f3f6b4e60c89ee5ba08a2f0c1addd5e07fc8
Alex Sukov <sukovsasha@gmail.com>
2016-11-07 18:52:46 +0300

    #5 All errors fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,45 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   respond_to :json, :html
-  # GET /posts
-  # GET /posts.json
+
   def index
     respond_with(@posts = Post.all)
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     respond_with(@post = Post.new)
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.create(post_params)
     respond_with(@post)
-
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     @post.update(post_params)
     respond_with(@post)
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_with(@post)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :body, :user_id)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
 end
