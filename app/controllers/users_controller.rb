127ecd0a9e561072933f775647b8e59630eded40
Alex Sukov <sukovsasha@gmail.com>
2016-12-05 13:40:56 +0300

    #13 Added view profile and list of all users. Configured routes. Ready to merge. Based on last development

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, except: ['index','new']
   end
-  devise_for :users
-  match '/users',   to: 'users#index',   via: 'get'
+  devise_for :users, path_prefix: 'd'
+  resources :users, only: :show
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
 end
