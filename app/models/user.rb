8dd582999f830ebda1bf0e66c9bd06e572ccb14c
Alex Sukov <sukovsasha@gmail.com>
2016-12-05 13:27:29 +0300

    #13 Added Forgot your password function via username. Mailer don't work!

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   #overwrite Devise's find_for_database_authentication method
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-      where(conditions.to_h).first
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
     end
     #make email to be case insensitive
     conditions[:email].downcase! if conditions[:email]
