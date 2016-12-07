64602eddd6d7b8612593cd879f82d165a6c2c3ae
AlexSukov <sukovsasha@gmail.com>
2016-12-07 10:57:46 +0300

    Userlist in profile (#13)
    * #13 Added username column to database. User can enter through username or email. Added views for User#index to show all users
    
    * #13 Added Forgot your password function via username. Mailer don't work!
    
    * #13 Added view profile and list of all users. Configured routes. Ready to merge. Based on last development
    
    * #13 Little changes
    
    * #13 Deleted all old code

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, except: ['index','new']
   end
-  devise_for :users
+  devise_for :users, path_prefix: 'd'
+  resources :users, only: :show
+  match '/users', to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get'
 end
