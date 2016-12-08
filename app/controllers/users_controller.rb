077793d8118e7dc292ff2a832d5e3136a821f41a
Alex Sukov <sukovsasha@gmail.com>
2016-12-08 16:12:50 +0300

    #16 Start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :set_comments, only: :show
   def index
     @users = User.all
   end
 
   def show
     @user = User.find_by_username(params[:id])
+  end
+
+  private
+
+  def set_comments
+    @comments = Comment.where(user_id: current_user.id)
+    @posts = Post.where(id: 37)
   end
 end
