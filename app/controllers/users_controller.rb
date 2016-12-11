09eb0017d74ab9c9c8d778f5a3717938ee3433df
Alex Sukov <sukovsasha@gmail.com>
2016-12-11 20:48:48 +0300

    #16 Work is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:id])
   end
-
-  private
-
-  def set_comments
-    @comments = Comment.where(user_id: current_user.id)
-    @posts = Post.where(id: 37)
-  end
 end
