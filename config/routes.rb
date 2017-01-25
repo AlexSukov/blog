daa299ced96ba04a1c748c6005ac37a15d4a82a4
Alex Sukov <sukovsasha@gmail.com>
2017-01-25 15:26:51 +0300

    #24 Made simple search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts do
+    collection {post :search, to: 'posts#index'}
     resources :comments, except: ['index','new']
   end
   devise_for :users, path_prefix: 'd'
