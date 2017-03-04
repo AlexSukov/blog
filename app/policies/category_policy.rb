ba19b2d8bcd9804997941873bcc85bb092e3fd64
vakyl9r <proskater.2008@gmail.com>
2017-03-04 16:47:03 +0300

    #32: DRY'd category policy.

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -10,10 +10,6 @@ class CategoryPolicy < ApplicationPolicy
     @current_user&.admin?
   end
 
-  def edit?
-    @current_user&.admin? || @current_user&.moderator?
-  end
-
   def update?
     @current_user&.admin? || @current_user&.moderator?
   end
