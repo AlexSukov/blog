9c5662e49f83cb08adfbdc958c0f0c26af1ac905
Alex Sukov <sukovsasha@gmail.com>
2017-12-14 19:18:52 +0300

    Fix 2

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
-
 module Blog
   class Application < Rails::Application
     # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
     config.i18n.default_locale = :ru
     config.i18n.locale = :ru
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
